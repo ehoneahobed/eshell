97da33b1602ba4d47ca78667aab094b870779f96
Ehoneah Obed <ehoneahobed@hotmail.com>
2022-11-10 11:47:30 +0000

    Add basic functionality - prompt, read, parse

---
+++ README.md
@@ -0,0 +1,19 @@
+# Eshell - A miniature version of Bash Shell
+This is a project I embarked on to help me revise C programming and also to share with others how they can build their own Shell.
+
+It started out as a series of blog posts but I look forward to making a video series on it as well.
+
+## How you can contribute
+I am open to suggestions and corrections to make it better. You can open an issue or make a pull request to contribute to the project.
+
+On the other hand, you can review the blog posts that I have written on this project and share your contributions in the form of comments on the blog.
+
+## How can you support me
+You can show me some love by:
+1. Following my blog [Ehoneah Obed](https://hashnode.com/@ehoneahobed)
+2. Subscribe to my [YouTube channel](https://youtube.com/@ehoneahobed?sub_confirmation=1) 
+
+## How to connect with me
+If you would love to reach out to me so we can chat about anything of interest to you or discuss any project, hit me up in my Twitter DM or send me an email:
+- [Twitter](https://ehoneahobed.com/twitter)
+- [Email](mailto:ehoneahobed@hotmail.com)
---
+++ main.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+int main(int ac, char **argv){
+    char *prompt = "(Eshell) $ ";
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    size_t n = 0;
+    ssize_t nchars_read;
+    const char *delim = " \n";
+    int num_tokens = 0;
+    char *token;
+    int i, counter;
+
+    /* declaring void variables */
+    (void)ac;
+
+    /* Create a loop for the shell's prompt */
+    while (1) {
+        printf("%s", prompt);
+        nchars_read = getline(&lineptr, &n, stdin);
+        /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
+        }
+
+        /* allocate space for a copy of the lineptr */
+        lineptr_copy = malloc(sizeof(char) * nchars_read);
+        if (lineptr_copy== NULL){
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        /* copy lineptr to lineptr_copy */
+        strcpy(lineptr_copy, lineptr);
+
+        /********** split the string (lineptr) into an array of words ********/
+        /* calculate the total number of tokens */
+        token = strtok(lineptr, delim);
+
+        while (token != NULL){
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        num_tokens++;
+
+        /* Allocate space to hold the array of strings */
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        /* Store each token in the argv array */
+        token = strtok(lineptr_copy, delim);
+
+        for (i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+        /* print the content of argv */
+        for (counter = 0; counter<num_tokens-1; counter++){
+            printf("%s\n", argv[counter]);
+        } 
+
+        /* free up allocated memory */ 
+        free(lineptr);
+
+    } 
+
+
+    return (0);
+}
